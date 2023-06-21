4299b0d94c0b6c6c2fe7ab2847f860e84c626f51
Victor Basov <vbasov@inpolus.ru>
2023-06-21 11:10:23 +0300

    Task 1633 init

--- 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /* 
 Клубок
@@ -15,31 +14,23 @@ public class Solution {
     public static List<Thread> threads = new ArrayList<>(5);
 
     static {
-        Thread thread1 = new Thread1();
-        Thread thread2 = new Thread2();
-        Thread thread3 = new Thread3();
-        Thread thread4 = new Thread4();
-        Thread thread5 = new Thread5();
-        threads.add(thread1);
-        threads.add(thread2);
-        threads.add(thread3);
-        threads.add(thread4);
-        threads.add(thread5);
-
-        for (Thread t : threads) {
-            t.start();
-        }
-
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
     }
 
     public static void main(String[] args) {
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
     public static class Thread1 extends Thread {
         @Override
         public void run() {
             while (true) {
-
             }
         }
     }
@@ -47,13 +38,10 @@ public class Solution {
     public static class Thread2 extends Thread {
         @Override
         public void run() {
-            Thread current = Thread.currentThread();
-            while (!current.isInterrupted()) {
-                try {
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    current.interrupt();
-                }
+            try {
+              throw new InterruptedException();
+            } catch (InterruptedException e) {
+                System.out.println(e);;
             }
         }
     }
@@ -61,46 +49,46 @@ public class Solution {
     public static class Thread3 extends Thread {
         @Override
         public void run() {
-            try {
-                while (true) {
-                    sleep(500);
-                    System.out.println("Ура");
+            while (true) {
+                System.out.println("Ура");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
         }
     }
 
     public static class Thread4 extends Thread implements Message {
-        Thread current = Thread.currentThread();
         @Override
         public void run() {
-            while (!current.isInterrupted()) {
-
-            }
+            while (!isInterrupted()) {}
         }
 
         @Override
         public void showWarning() {
-            if (current.isAlive()) {
-                current.interrupt();
-            }
+            this.interrupt();
         }
     }
 
     public static class Thread5 extends Thread {
-        int sum = 0;
         @Override
         public void run() {
-            Scanner scanner = new Scanner(System.in);
-            String s = scanner.nextLine();
-            while (!s.equals("N")) {
-                sum += Integer.parseInt(s);
-                s = scanner.nextLine();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int sum = 0;
+            while (true) {
+                try {
+                    String input = reader.readLine();
+                    if (input.equals("N")) {
+                        break;
+                    }
+                    sum += Integer.parseInt(input);
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
             }
             System.out.println(sum);
         }
     }
-
 }
