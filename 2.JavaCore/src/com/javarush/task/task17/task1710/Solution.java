372b9cea3701dc8765079bb2cd0b32dfa5ab17a0
Victor Basov <vbasov@inpolus.ru>
2023-07-07 18:01:10 +0300

    Task 1710 solved, 1711 init

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,5 +22,49 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-YYYY", Locale.ENGLISH);
+        SimpleDateFormat formatIn = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        Person person;
+        switch (args[0]) {
+            case "-c" :
+                // TODO проверка на наличие всех параметров
+                try {
+                    Date date = formatIn.parse(args[3]);
+                    person = args[2].equals("м") ? Person.createMale(args[1], date) : Person.createFemale(args[1], date);
+                    allPeople.add(person);
+                    System.out.println(allPeople.indexOf(person));
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "-r" :
+                // TODO проверка на наличие всех параметров
+                person = allPeople.get(Integer.parseInt(args[1]));
+                String sex = person.getSex().equals(Sex.MALE) ? "м" : "ж";
+                System.out.println(person.getName() + " " + sex + " " + formatter.format(person.getBirthDate()));
+                break;
+            case "-u" :
+                // TODO проверка на наличие всех параметров
+                try {
+                    Date date = formatIn.parse(args[4]);
+                    person = allPeople.get(Integer.parseInt(args[1]));
+                    person.setName(args[2]);
+                    person.setSex((args[3].equals("м") ? Sex.MALE : Sex.FEMALE));
+                    person.setBirthDate(date);
+                    allPeople.set(Integer.parseInt(args[1]), person);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+
+                break;
+            case "-d" :
+                // TODO проверка на наличие всех параметров
+                person = allPeople.get(Integer.parseInt(args[1]));
+                person.setName(null);
+                person.setSex(null);
+                person.setBirthDate(null);
+                allPeople.set(Integer.parseInt(args[1]), person);
+                break;
+        }
     }
 }
