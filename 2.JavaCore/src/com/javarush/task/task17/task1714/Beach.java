26623e81b64775ee2323aed130165726900e0cc9
Victor Basov <vbasov@inpolus.ru>
2023-07-11 14:27:26 +0300

    Task 1714 solved, 1716 init

--- 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -4,7 +4,7 @@ package com.javarush.task.task17.task1714;
 Comparable
 */
 
-public class Beach {
+public class Beach implements Comparable<Beach> {
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -15,31 +15,49 @@ public class Beach {
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
     }
 
     public static void main(String[] args) {
+        Beach beach1 = new Beach("name1", 10.3f, 5);
+        Beach beach2 = new Beach("name2", 10.1f, 5);
+        System.out.println(beach1.compareTo(beach2));
+    }
 
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int result = 0;
+        if (this.quality > o.quality) {
+            result +=1;
+        } else if (this.quality < o.quality) {
+            result--;
+        }
+        if (this.distance < o.distance) {
+            result +=1;
+        } else if (this.distance > o.distance) {
+            result--;
+        }
+        return result;
     }
 }
