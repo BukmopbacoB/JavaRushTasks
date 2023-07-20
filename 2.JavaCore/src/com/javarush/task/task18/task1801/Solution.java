1667e7c33dff940ead3de84c6418e070d55e18bf
Victor Basov <vbasov@inpolus.ru>
2023-07-20 10:48:15 +0300

    Task 1801 solved, 1802 init

--- 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task18.task1801;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /* 
@@ -10,5 +11,17 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+//        String dirName = "C:\\Users\\bukmo\\Study\\JavaRush\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task18\\task1801\\";
+
+        int max = 0;
+        try (FileInputStream inputStream = new FileInputStream(/*dirName + */fileName)) {
+            while (inputStream.available() > 0) {
+                int in = inputStream.read();
+                if (in > max) max = in;
+            }
+        }
+        System.out.println(max);
     }
 }
