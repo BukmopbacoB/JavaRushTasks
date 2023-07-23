d0693224f6e561edd833a1f5b0a9bb81a6764b3e
Victor Basov <vbasov@inpolus.ru>
2023-07-23 13:37:21 +0300

    Task 1803 solved, 1804 init

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -11,5 +11,24 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        int max = 0;
+        int[] rep = new int[256];
+        try (FileInputStream inputStream = new FileInputStream(fileName)) {
+            while (inputStream.available() > 0) {
+                int in = inputStream.read();
+                rep[in] += 1;
+                if (rep[in] > max) max = rep[in];
+            }
+        }
+//        System.out.println(max);
+        for (int i = 0; i < 256; i++) {
+            if (rep[i] == max) {
+                System.out.print(/*(char)*/ i + " ");
+            }
+        }
+        reader.close();
     }
 }
