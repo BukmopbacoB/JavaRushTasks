dad925c10bafb7dc0c3d27a58543cfe5e5734718
Victor Basov <vbasov@inpolus.ru>
2023-07-25 10:29:34 +0300

    Task 1804, 1805 - solved, 1806 - init

--- 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
@@ -1,34 +1,28 @@
-package com.javarush.task.task18.task1804;
+package com.javarush.task.task18.task1805;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 /* 
-Самые редкие байты
+Сортировка байт
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-        
-        int[] repeat = new int[256];
+
+        int[] bytes = new int[256];
         try (FileInputStream inputStream = new FileInputStream(fileName)) {
             while (inputStream.available() > 0) {
-                repeat[inputStream.read()] += 1;
+                bytes[inputStream.read()]++;
             }
         }
-        int min = Integer.MAX_VALUE;
-        for (int b : repeat) {
-            if (b > 0 && b < min) min = b;
-        }
-
-        for (int i = 0; i < repeat.length; i++) {
-            if (repeat[i] == min) {
-                System.out.print(i + " ");
-            }
+        for (int i =0; i < bytes.length; i++) {
+            if (bytes[i] > 0) System.out.print(i + " ");
         }
     }
 }
