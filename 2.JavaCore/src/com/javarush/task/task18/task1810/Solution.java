4e7b4b04b9552be668036e925c5d595884645323
Victor Basov <vbasov@inpolus.ru>
2023-07-31 14:01:03 +0300

    Task 1810 - solved, 1811 - init

--- 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -10,8 +10,16 @@ DownloadException
 */
 
 public class Solution {
-    public static void main(String[] args) throws DownloadException {
+    public static void main(String[] args) throws DownloadException, IOException {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String fileName = reader.readLine();
+
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+                if (fileInputStream.available() < 1000) throw new DownloadException();
+            }
+        }
     }
 
     public static class DownloadException extends Exception {
